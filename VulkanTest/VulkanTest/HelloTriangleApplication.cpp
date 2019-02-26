59039f9e3c863da9ed654f87e70a4bc69a212d69
Sondre Eide Vik <sondre_vik@hotmail.com>
2019-02-26 20:43:15 +0100

    feature(PhysicalDevice) pick physical device
    we need to pick a GPU that meets our needs in this project. This we do by seeing if it is a GPU, supports geometry shaders and supports graphics que family
    
    #5

--- VulkanTest/VulkanTest/HelloTriangleApplication.cpp
+++ VulkanTest/VulkanTest/HelloTriangleApplication.cpp
@@ -48,6 +48,7 @@ void HelloTriangleApplication::initWindow()
 void HelloTriangleApplication::initVulkan() {
 	createInstance();
 	setupDebugCallback();
+	pickPhysicalDevice();
 }
 
 void HelloTriangleApplication::setupDebugCallback()
@@ -135,6 +136,30 @@ void HelloTriangleApplication::createInstance()
 
 }
 
+void HelloTriangleApplication::pickPhysicalDevice()
+{
+	VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+	uint32_t deviceCount = 0;
+	vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+	if (deviceCount == 0) {
+		throw std::runtime_error("failed to find GPUs with Vulkan support!");
+	}
+
+	std::vector<VkPhysicalDevice> devices(deviceCount);
+	vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+	for (const auto& device : devices) {
+		if (isDeviceSuitable(device)) {
+			physicalDevice = device;
+			break;
+		}
+	}
+
+	if (physicalDevice == VK_NULL_HANDLE) {
+		throw std::runtime_error("failed to find a suitable GPU!");
+	}
+
+}
+
 bool HelloTriangleApplication::checkValidationLayerSupport()
 {
 	uint32_t layerCount;
@@ -159,6 +184,42 @@ bool HelloTriangleApplication::checkValidationLayerSupport()
 	}
 
 	return true;
+}
+
+bool HelloTriangleApplication::isDeviceSuitable(VkPhysicalDevice device)
+{
+	VkPhysicalDeviceProperties deviceProperties;
+	VkPhysicalDeviceFeatures deviceFeatures;
+	vkGetPhysicalDeviceProperties(device, &deviceProperties);
+	vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
+	QueueFamilyIndices indices = findQueueFamilies(device);
+
+	return deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU &&
+		deviceFeatures.geometryShader && indices.isComplete();
+}
+
+QueueFamilyIndices HelloTriangleApplication::findQueueFamilies(VkPhysicalDevice device)
+{
+	QueueFamilyIndices indices;
+
+	uint32_t queueFamilyCount = 0;
+	vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+
+	std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+	vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+	int i = 0;
+	for (const auto& queueFamily : queueFamilies) {
+		if (queueFamily.queueCount > 0 && queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT)
+			indices.graphicsFamily = i;
+
+		if (indices.isComplete())
+			break;
+
+		i++
+	}
+
+	return indices;
 }
 
 std::vector<const char*> HelloTriangleApplication::getRequiredExtensions()
--- VulkanTest/VulkanTest/HelloTriangleApplication.h
+++ VulkanTest/VulkanTest/HelloTriangleApplication.h
@@ -4,10 +4,10 @@
 
 #include <iostream>
 #include <stdexcept>
-#include <functional>
-#include <cstdlib>
 #include <vector>
 #include <cstring>
+#include <cstdlib>
+#include <optional>
 
 const int WIDTH = 800;
 const int HEIGHT = 600;
@@ -21,6 +21,13 @@ const bool enableValidationLayers = false;
 #else
 const bool enableValidationLayers = true;
 #endif // NDEBUG
+
+struct QueueFamilyIndices {
+	std::optional<uint32_t> graphicsFamily;
+	bool isComplete() {
+		return graphicsFamily.has_value();
+	}
+};
 
 class HelloTriangleApplication {
 public: 
@@ -38,11 +45,15 @@ private:
 	void mainLoop();
 	void cleanup();
 	void createInstance();
+	void pickPhysicalDevice();
 	bool checkValidationLayerSupport();
+	bool isDeviceSuitable(VkPhysicalDevice device);
+	QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);
 	std::vector<const char*> getRequiredExtensions();
 	static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
 		VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, // severity. Can be one of four: verbose, info, warning or error
 		VkDebugUtilsMessageTypeFlagsEXT messageType, // type. can have one of three values: general, validation or performance
 		const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, // data, contains info on error
 		void* pUserData); // allowes for input of own data (pointer to HelloTriangleApplication class to check fields)
+
 };
